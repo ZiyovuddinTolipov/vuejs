ca7cf8befdaed05c3616a613f8f28da3ff6cbb02
tolipov <ziyovuddin.developer@gmail.com>
2023-12-23 20:59:23 +0500

    cunter app

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
-import './assets/main.css'
-
+import "@/assets/main.css"
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import App from '@/components/app/App.vue'
 
 const app = createApp(App)
 
-app.use(router)
 
 app.mount('#app')
